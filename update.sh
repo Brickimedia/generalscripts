4d74f3d441672a4ceaf3ffb41d1916c7c87c5f99
Lewis Cawte <lewis@lewiscawte.me>
2013-11-17 21:53:01 +0000

    New repos
    Add Quartz and Refreshed to update.sh.

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 echo "Brickimedia automatic updater"
 echo "#############################"
 
-locations=("/var/www" "/media/MediaWikiChat" "/media/NewTalkGlobal" "/media/SocialProfile" "/var/www/wiki/skins/Refreshed" "/media/Custard" "/media/GlobalContribs" "/var/www/wiki/extensions/NumberOfComments")
+locations=("/var/www" "/media/MediaWikiChat" "/media/NewTalkGlobal" "/media/SocialProfile" "/var/www/wiki/skins/Refreshed" "/media/Custard" "/media/GlobalContribs" "/var/www/wiki/extensions/NumberOfComments" "/var/www/wiki/skins/Quartz" "/var/www/wiki/skins/DeepSea" )
 
 for path in ${locations[*]}
 do
