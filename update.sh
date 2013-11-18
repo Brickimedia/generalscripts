3ff732d16257875ad33f77453da8a39a14c16211
George Barnick <george.barnick@brickcraft.me>
2013-11-17 22:21:02 -0500

    Update update.sh
    Reorganising the order that the script updates each repository so that all the skins are updated together instead of it doing some skins, then doing other repositories, then doing more skins.

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 echo "Brickimedia automatic updater"
 echo "#############################"
 
-locations=("/var/www" "/media/MediaWikiChat" "/media/NewTalkGlobal" "/media/SocialProfile" "/var/www/wiki/skins/Refreshed" "/media/Custard" "/media/GlobalContribs" "/var/www/wiki/extensions/NumberOfComments" "/var/www/wiki/skins/Quartz" "/var/www/wiki/skins/DeepSea" )
+locations=("/var/www" "/media/MediaWikiChat" "/media/NewTalkGlobal" "/media/SocialProfile" "/var/www/wiki/skins/Refreshed" "/media/Custard" "/var/www/wiki/skins/Quartz" "/var/www/wiki/skins/DeepSea" "/media/GlobalContribs" "/var/www/wiki/extensions/NumberOfComments" )
 
 for path in ${locations[*]}
 do
