a5f0a32dd5e69d4bc469cc28e6b9696961db0fd7
UltrasonicNXT <adamr_carter@btinternet.com>
2013-12-12 17:54:47 +0000

    No more 1.22

--- update.sh
+++ update.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-echo "Updating Brickimedia 1.20"
-echo "#############################"
-
-locations=("/var/www" "/media/MediaWikiChat" "/media/NewTalkGlobal" "/media/SocialProfile" "/var/www/wiki/skins/Refreshed" "/media/Custard" "/var/www/wiki/skins/Quartz" "/var/www/wiki/skins/DeepSea" "/media/GlobalContribs" "/var/www/wiki/extensions/NumberOfComments" )
-
-for path in ${locations[*]}
-do
-  file=`echo $path | rev | cut -d/ -f1 | rev`
-  echo "Running update for "$file"..."
-  cd $path
-  git pull
-done
-
-echo "Complete!"
-
-#1.22 updates
-echo
 echo "Updating Brickimedia 1.22"
 echo "#############################"
 
@@ -33,6 +16,7 @@ echo "Updating extensions"
 cd /var/www/core/extensions
 git pull
 git submodule init
+git submodule sync
 git submodule update
 
 echo "Updating LocalSettings"
