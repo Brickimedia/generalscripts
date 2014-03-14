ef6e6bc05bb9708b52b0b66230a8ab1d58165d95
George Barnick <george.barnick@brickcraft.me>
2014-03-14 11:40:34 -0400

    Adding necessary configuration for Brickidata

--- content-backup.sh
+++ content-backup.sh
@@ -7,7 +7,7 @@ mkdir -p /media/backups/$now
 #Source file for database passwords
 source /media/backups/pass.sh
 
-databases=(shared meta en customs stories cuusoo admin dev globalblocking)
+databases=(shared meta en customs stories cuusoo admin dev globalblocking data)
 
 echo "backing up dbs"
 
